c30a0f02c71296f5dfe353df4418a130bdb3c350
Vladimir Amelkin <v.amelkin@scloud.ru>
2026-03-19 22:39:52 +0300

    added main thread

--- ios/Plugin/CapacitorSwipeBackPlugin.swift
+++ ios/Plugin/CapacitorSwipeBackPlugin.swift
@@ -3,17 +3,33 @@ import Capacitor
 
 @objc(CapacitorSwipeBackPlugin)
 public class CapacitorSwipeBackPlugin: CAPPlugin {
-    @objc func enable(_ call: CAPPluginCall) {
-        webView?.allowsBackForwardNavigationGestures = true;
-        call.resolve([
-            "status": "enable"
-        ])
+  @objc func enable(_ call: CAPPluginCall) {
+    guard let webView else {
+      call.reject("WebView not initialized")
+      return
     }
     
-    @objc func disable(_ call: CAPPluginCall) {
-        webView?.allowsBackForwardNavigationGestures = false;
-        call.resolve([
-            "status": "disable"
-        ])
+    DispatchQueue.main.async {
+      webView.allowsBackForwardNavigationGestures = true;
     }
+    
+    call.resolve([
+      "status": "enable"
+    ])
+  }
+  
+  @objc func disable(_ call: CAPPluginCall) {
+    guard let webView else {
+      call.reject("WebView not initialized")
+      return
+    }
+    
+    DispatchQueue.main.async {
+      webView.allowsBackForwardNavigationGestures = false;
+    }
+    
+    call.resolve([
+      "status": "disable"
+    ])
+  }
 }
